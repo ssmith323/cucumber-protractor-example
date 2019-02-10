ae8c91f1abaf90d5fc04006803215cf8bc475022
s.j.smith <s.j.smith@accenture.com>
2019-02-09 19:40:08 -0500

    enhancing login functionality

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { LoginService } from '../shared/login.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  isLoggedInSubscription: Subscription;
+  isLoggedIn = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private loginService: LoginService) { }
+
+  ngOnInit() {
+    this.isLoggedInSubscription = this.loginService.isLoggedIn.subscribe(data => this.isLoggedIn = data);
+  }
 
 }
--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { LoginInfo } from './models/login-info';
 
@@ -6,6 +7,7 @@ import { LoginInfo } from './models/login-info';
   providedIn: 'root'
 })
 export class LoginService {
+  isLoggedIn = new Subject<boolean>();
 
   constructor() { }
 
@@ -13,6 +15,7 @@ export class LoginService {
     return new Promise((resolve, reject) => {
       if (login.username === 'test.user' && login.password === 'password') {
         setTimeout(resolve, 0, 'promise worked');
+        this.isLoggedIn.next(true);
       } else {
         setTimeout(reject, 1500, 'rejected');
       }
